5ab84c01ebc711bbc1cc9ae5e93e34d662beb109
Candy Wang <candywang013@gmail.com>
2019-07-09 19:46:22 -0700

    Create 100 mock data

--- db/seed.js
+++ db/seed.js
@@ -2,21 +2,38 @@ const db = require('./index.js');
 const Review = require('./Review.js');
 const faker = require('faker');
 
-let randomName = faker.name.findName();
-
-let firstName = randomName => {
-  var first = randomName.split(' ');
-  return first[0];
+let year = () => {
+  let y = faker.date.past().toString().split(' ');
+  return y[3];
 }
 
-const sampleReviews = [
-  {
-    firstName: firstName(randomName),
-    date: 'June 1st',
-    ratings: [0, 0, 0, 0, 0, 0],
-    comment: 'Awesome host!'
+let monthYear = faker.date.month() + ' ' + year();
+
+let rating = () => {
+  let result = [];
+  
+  for (let i = 0; i < 6; i++) {
+    result.push(faker.random.number(5));
   }
-]
+  
+  return result;
+};
+
+const sampleReviews = () => {
+  let reviews = [];
+  let review = {
+    firstName: faker.name.firstName(),
+    date: monthYear,
+    ratings: rating(),
+    comment: faker.lorem.paragraph()
+  }
+
+  for (let i = 0; i < 100; i++) {
+    reviews[i] = review;
+  }
+
+  return reviews;
+}
 
 const insertSampleReviews = function() {
   Review.create(sampleReviews)
